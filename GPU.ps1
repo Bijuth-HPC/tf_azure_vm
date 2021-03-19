914d87cb8d75de72bb3afdf167fd4bec6c2fb035
bijuth-MSFT-hpc <79228201+bijuth-MSFT-hpc@users.noreply.github.com>
2021-03-19 16:45:57 -0500

    Add files via upload

---
+++ GPU.ps1
@@ -0,0 +1,7 @@
+$url = "https://download.microsoft.com/download/4/8/d/48d2d45b-bebc-44ad-9c58-e0b79a9d4ff2/461.09_grid_win10_server2016_server2019_64bit_azure_swl.exe"
+$outpath = "$PSScriptRoot/GPU.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+$args = @("Comma","Separated","Arguments")
+Start-Process -Filepath "$PSScriptRoot/GPU.exe " -ArgumentList $args
