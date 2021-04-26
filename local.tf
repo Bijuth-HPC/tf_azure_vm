1efe0f095bc7c750361018f66d71a8e3c761a708
biju888 <57495769+biju888@users.noreply.github.com>
2021-04-26 14:12:38 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,29 +1,5 @@
 locals { 
 nsgrules = { 
-    HTTPS = {
-      name                       = "HTTPS"
-      priority                   = 1010
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range    = "443"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
- 
-    HTTP = {
-      name                       = "HTTP"
-      priority                   = 1020
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_port_range          = "*"
-      destination_port_range     = "80"
-      source_address_prefix      = "*"
-      destination_address_prefix = "*"
-    }
- 
     SSH = {
       name                       = "SSH"
       priority                   = 1030
